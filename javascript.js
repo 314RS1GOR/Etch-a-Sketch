f87c42b842fc1eb236684f836ba86279a1c1dabc
314RS1GOR <pierre.momplaisir@tuta.io>
2024-10-21 10:44:40 +0200

    added the randomized colours mode

--- javascript.js
+++ javascript.js
@@ -1,7 +1,11 @@
 const container = document.querySelector(".container");
 const defaultNumberOfCases = 16;
+
 const resetButton = document.querySelector("#reset");
 const gridbutton = document.querySelector("#new-grid");
+const colorButton = document.querySelector("#randomize-colours");
+
+const colorArray = ['A', 'B', 'C','D','E','F',0,1,2,3,4,5,6,7,8,9];
 
 BuildGrid = function (cases){
     for (let i=0; i<cases; i++){
@@ -12,6 +16,36 @@ BuildGrid = function (cases){
             const square = document.createElement("div");
             square.addEventListener("mousemove", ()=>{
                 square.classList.add("darkened");
+            });
+            square.classList.add("square");
+            newColumn.appendChild(square);
+            }
+    }
+}
+
+ColorPicker = function (){
+    let myColor = "#";
+
+    for (i=0; i<6; i++){
+        let colorHolder = colorArray[(Math.floor(Math.random() * colorArray.length))];
+        myColor = myColor + colorHolder;
+    }
+    
+    console.log(myColor);
+
+    return myColor;
+}
+
+BuildColorGrid = function (cases){
+    for (let i=0; i<cases; i++){
+        const newColumn = document.createElement("div");
+        newColumn.classList.add("vertical");
+        container.appendChild(newColumn);
+        for (j=0; j<cases; j++){
+            const square = document.createElement("div");
+            square.addEventListener("mousemove", ()=>{
+                // let couleur = ColorPicker();
+                square.style.backgroundColor = ColorPicker();
             });
             square.classList.add("square");
             newColumn.appendChild(square);
@@ -51,7 +85,6 @@ FixSizeOfSquares = function (){
     const heightOfSquares = 640/(Math.sqrt(numberOfSquares.length));
 
     for (i=(numberOfSquares.length-1); i >= 0; i--){
-        /* console.log(numberOfSquares); */
         numberOfSquares[i].style.height = heightOfSquares+"px";
         numberOfSquares[i].style.width = heightOfSquares+"px";
     }
@@ -68,8 +101,14 @@ gridbutton.addEventListener("click", ()=>{
     FixSizeOfSquares();
 })
 
+colorButton.addEventListener("click", ()=>{
+    RemoveGrid();
+    BuildColorGrid(defaultNumberOfCases);
+});
+
 
 
 BuildGrid(defaultNumberOfCases);
+
 
 
